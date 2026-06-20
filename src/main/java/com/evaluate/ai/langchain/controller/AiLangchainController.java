2a60c08146f20c72b169a28bede165cde65298a8
Manoj Suthar <manojjangid321@gmail.com>
2026-06-20 12:45:56 +0530

    CORS origin added

--- src/main/java/com/evaluate/ai/langchain/controller/AiLangchainController.java
+++ src/main/java/com/evaluate/ai/langchain/controller/AiLangchainController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api/langchain")
 @RestController
 public class AiLangchainController {
